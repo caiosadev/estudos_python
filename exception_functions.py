df420a1abf5bd07dd6ab72378cb23939340541f7
caiosadev <caio@visuals.com.br>
2026-09-26 10:43:57 -0300

    Reoganizando arquivos e pastas

--- exception_functions.py
+++
@@ -1,8 +0,0 @@
-try:
-    a = input("escreva um número:")
-    b = input("escreva outro número:")
-    a = int(a)
-    b = int(b)
-    print(a / b)
-except (ZeroDivisionError, ValueError): #captura divisão por zero e digitação de string
-    print("Entrada inválida.")
